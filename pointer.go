6c9eb12f4b233fd7a9af9ade77e905e80e4912cd
y-u-y-a <yuya.ymd00@gmail.com>
2020-08-12 10:13:43 +0900

    コメントアウト

--- pointer.go
+++ pointer.go
@@ -1,27 +1,27 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func pointer() {
+// func pointer() {
 
-	// ポインタ ====================================================================
+// 	// ポインタ ====================================================================
 
-	// 変数(値を指した箱)
-	num := 100
-	fmt.Println(&num) // アドレス取得
+// 	// 変数(値を指した箱)
+// 	num := 100
+// 	fmt.Println(&num) // アドレス取得
 
-	// ポインタ変数(アドレスを指した箱)
-	p := &num       // アドレス格納
-	fmt.Println(*p) // アドレス内の値取得
+// 	// ポインタ変数(アドレスを指した箱)
+// 	p := &num       // アドレス格納
+// 	fmt.Println(*p) // アドレス内の値取得
 
-	// new(空のメモリを確保)
-	newNum := new(int)
-	fmt.Println(&newNum)
+// 	// new(空のメモリを確保)
+// 	newNum := new(int)
+// 	fmt.Println(&newNum)
 
-	var newNum2 *int
-	fmt.Println(newNum2) // nill
-}
+// 	var newNum2 *int
+// 	fmt.Println(newNum2) // nill
+// }
 
-func main() {
-	pointer()
-}
+// func main() {
+// 	pointer()
+// }
